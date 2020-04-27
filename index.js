a4e162952d311da272d98e6ac7b06ac53f2e760a
Ernst Widerberg <ernstwi@kth.se>
2020-04-27 20:18:39 +0200

    new: Add --no-edit flag

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ let { Server } = require('./src/server.js');
 
 function error(stream) {
     stream.write(`Usage:
-  ${__binname} new [<year> <month> <day> [<hour> [<minute> [<second>]]]]
+  ${__binname} new [--no-edit] [<year> <month> <day> [<hour> [<minute> [<second>]]]]
   ${__binname} server [--port <number>]`);
     process.exit(stream == process.stderr ? 1 : 0);
 }
@@ -22,24 +22,43 @@ if (process.argv.length < 3 || process.argv[2] == '--help') {
 
 switch (process.argv[2]) {
     case 'new':
-        let argn = process.argv.length;
+        let args = {
+            'noEdit': false,
+            'date': []
+        };
+        for (let i = 3; i < process.argv.length; i++) {
+            if (process.argv[i] == '--no-edit') {
+                args['noEdit'] = true;
+                continue;
+            }
 
-        if (!(argn == 3 || argn >= 6)) {
+            if (/\d{4}/.test(process.argv[i])) {
+                if (i+2 >= process.argv.length)
+                    error(process.stderr);
+                while (i < process.argv.length) {
+                    args.date.push(process.argv[i++]);
+                }
+                continue;
+            }
             error(process.stderr);
         }
+        if (args.date.length > 6)
+            error(process.stderr);
+        if (args.date.length >= 2)
+            args.date[1]--; // Month
 
-        let date = new Date();
-
-        if (argn >= 6) {
-                process.argv[4]--; // Month number
-            date = new Date(Date.UTC(...process.argv.splice(3)));
+        let date;
+        if (args.date.length == 0) {
+            date = new Date();
+            date.setUTCHours(date.getHours());
+        } else {
+            date = new Date(Date.UTC(...args.date));
         }
 
         let dir = `content/${dateFormat(date, 'UTC:yyyy/mm/dd')}`;
-
-        let file = (argn == 3 || argn > 6) ?
-            `${dir}/${dateFormat(date, 'UTC:HH-MM-ss".md"')}` :
-            `${dir}/allday.md`;
+        let file = (args.date.length == 3) ?
+            `${dir}/allday.md` :
+            `${dir}/${dateFormat(date, 'UTC:HH-MM-ss".md"')}`;
 
         if (fs.existsSync(file)) {
             process.stderr.write(`${__binname}: \x1b[31mError\x1b[0m: ${file} already exists.`);
@@ -49,7 +68,10 @@ switch (process.argv[2]) {
         fs.mkdirSync(dir, { recursive: true });
         fs.writeFileSync(file, '');
 
-        cp.spawn(process.env.EDITOR, [file], { stdio: 'inherit' });
+        if (args.noEdit)
+            console.log(file);
+        else
+            cp.spawn(process.env.EDITOR, [file], { stdio: 'inherit' });
         return;
     case 'server':
         let port = 3004;
