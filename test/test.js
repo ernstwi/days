144d9717cb153d98e2fa5663f35b07ea9ee5a77b
Ernst Widerberg <ernstwi@kth.se>
2020-07-24 19:41:59 +0200

    Add partial testing
    Using mocha and puppeteer

---
+++ test/test.js
@@ -0,0 +1,163 @@
+let assert = require('assert');
+let cp = require('child_process');
+let fs = require('fs');
+let path = require('path');
+
+let puppeteer = require('puppeteer');
+
+let { Server } = require('../src/server.js');
+
+before(function() {
+    global.__basedir = path.join(__dirname, '..');
+    global.__binname = 'days';
+    global.__favoritesFile = '.fav';
+    global.__title = 'days';
+
+    if (!Number.prototype.zeropad) {
+        Number.prototype.zeropad = function(width) {
+            let res = this.toString();
+            return res.length >= width ? res :
+                new Array(width - res.length + 1).join('0') + res;
+        }
+    }
+
+    if (!Array.prototype.last) {
+        Array.prototype.last = function() {
+            return this[this.length - 1];
+        }
+    }
+
+    if (!String.prototype.lines) {
+        String.prototype.lines = function() {
+            let res = this.split('\n');
+
+            // Trailing newline
+            if (res[res.length-1] == '') {
+                res.splice(-1, 1);
+            }
+
+            return res;
+        }
+    }
+
+    cp.execSync('npm install -g');
+    process.chdir('test');
+});
+
+describe('CLI', function() {
+    describe('new', function() {
+        context('with a specified date', function() {
+            afterEach(function() {
+                cp.execSync('rm -rf content');
+            });
+
+            context('no collision', function() {
+                it('should create a new file', function() {
+                    cp.execSync('days new --no-edit 1992 07 15 00 00 00');
+                    fs.accessSync('content/1992/07/15/00-00-00.md');
+                });
+            });
+            context('collision', function() {
+                it('should not overwrite an existing file', function() {
+                    cp.execSync('echo "Hello, world!" > $(days new --no-edit 1992 07 15 00 00 00)');
+                    assert.throws(() => {
+                        cp.execSync('days new --no-edit 1992 07 15 00 00 00', { stdio: 'ignore' })
+                    });
+                    assert(fs.readFileSync('content/1992/07/15/00-00-00.md', 'utf8') == 'Hello, world!\n');
+                });
+            });
+        });
+    });
+
+    describe('merge', function() {
+        beforeEach(function() {
+            fs.mkdirSync('source');
+            fs.mkdirSync('target');
+            process.chdir('source');
+            cp.execSync('echo "a reference to /image.png" > $(days new --no-edit 1992 07 15 00 00 00)');
+            fs.mkdirSync('assets/subdir', { recursive: true });
+            cp.execSync('echo "image data in source dir" > assets/image.png');
+            process.chdir('../target');
+        });
+
+        afterEach(function() {
+            process.chdir('..');
+            cp.execSync('rm -rf source target');
+        });
+
+        describe('path', function() {
+            context('no conflict', function() {
+                it('should merge all posts and assets', function() {
+                    cp.execSync('days merge ../source');
+                    assert(fs.readFileSync('content/1992/07/15/00-00-00.md', 'utf8') == 'a reference to /image.png\n');
+                });
+            });
+
+            context('post conflict', function() {
+                it('should not overwrite an existing post', function() {
+                    cp.execSync('echo "new content in target dir" > $(days new --no-edit 1992 07 15 00 00 00)');
+                    cp.execSync('days merge ../source', { stdio: 'ignore' });
+                    assert(fs.readFileSync('content/1992/07/15/00-00-00.md', 'utf8') == 'new content in target dir\n');
+                });
+            });
+
+            context('asset conflict', function() {
+                beforeEach(function() {
+                    fs.mkdirSync('assets');
+                    cp.execSync('echo "image data in target dir" > assets/image.png');
+                });
+
+                context('no --resolve', function() {
+                    it('should ignore the colliding merged asset', function() {
+                        cp.execSync('days merge ../source', { stdio: 'ignore' });
+                        assert(fs.readFileSync('assets/image.png', 'utf8') == 'image data in target dir\n');
+                    });
+                });
+
+                context('--resolve', function() {
+                    it('should rename the colliding merged asset', function() {
+                        cp.execSync('days merge --resolve ../source', { stdio: 'ignore' });
+                        assert(fs.readFileSync('assets/image.png', 'utf8') == 'image data in target dir\n');
+                        assert(fs.readFileSync('assets/image-0.png', 'utf8') == 'image data in source dir\n');
+                        assert(fs.readFileSync('content/1992/07/15/00-00-00.md', 'utf8') == 'a reference to /image-0.png\n');
+                    });
+                });
+            });
+        });
+    });
+});
+
+describe('Web', function() {
+    let server;
+
+    before(async function() {
+        cp.execSync('days new --no-edit 2020 01 01 12 00 00');
+        server = new Server();
+        await server.run(3004);
+    });
+
+    after(async function() {
+        await server.close();
+        cp.execSync('rm -rf content');
+    });
+
+    describe('edit post', function() {
+        it('edit post', async function() {
+            let browser = await puppeteer.launch();
+            let page = await browser.pages().then(pages => pages[0]);
+            await page.goto('http://localhost:3004/2020/01/01/12/00/00/edit');
+            await page.type('textarea', 'Hello, world!');
+            await Promise.all([
+                page.waitForNavigation(),
+                page.click('input')
+            ]);
+            let text = await page.evaluate(() => {
+                return document.getElementsByClassName('post-body')[0].innerText;
+            });
+            await browser.close();
+
+            assert(text == 'Hello, world!');
+            assert(fs.readFileSync('content/2020/01/01/12-00-00.md', 'utf8') == 'Hello, world!\n');
+        });
+    });
+});
