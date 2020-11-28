b65b049b32d2e987c344dae941af2ba5ac4ae8ef
Ernst Widerberg <ernstwi@kth.se>
2020-11-29 00:16:38 +0100

    Add video buttons
    Controls, Slow motion, Audio, mpv

--- static/client.js
+++ static/client.js
@@ -3,13 +3,43 @@ function fav(elem, id) {
     fetch(`${id}?favorite`);
 }
 
-Array.from(document.getElementsByTagName('video')).forEach(e => {
-    e.muted = true;
-    e.loop = true;
-    e.autoplay = true;
-    e.onclick = function() {
-        this.muted = !this.muted;
+Array.from(document.getElementsByTagName('video')).forEach(video => {
+    video.muted = true;
+    video.loop = true;
+    video.autoplay = true;
+
+    let wrapper = document.createElement('div');
+    wrapper.className = 'video-wrapper';
+    video.replaceWith(wrapper);
+    wrapper.appendChild(video);
+
+    let controls = document.createElement('button');
+    controls.innerHTML = 'Controls';
+    controls.onclick = function() {
+        video.controls = !video.controls;
     };
+    wrapper.appendChild(controls);
+
+    let slow = document.createElement('button');
+    slow.innerHTML = 'Slow motion';
+    slow.onclick = function() {
+        video.playbackRate = video.playbackRate == 1 ? 0.25 : 1;
+    };
+    wrapper.appendChild(slow);
+
+    let audio = document.createElement('button');
+    audio.innerHTML = 'Audio';
+    audio.onclick = function() {
+        video.muted = !video.muted;
+    };
+    wrapper.appendChild(audio);
+
+    let mpv = document.createElement('button');
+    mpv.innerHTML = 'mpv';
+    mpv.onclick = function() {
+        fetch(`/mpv?${video.src}`);
+    };
+    wrapper.appendChild(mpv);
 });
 
 Array.from(document.getElementsByTagName('audio')).forEach(e => {
