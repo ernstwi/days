69a51186eb97fe0aa51ab76114ca4e52c7bee49b
Ernst Widerberg <ernstwi@kth.se>
2021-04-01 13:57:10 +0200

    Update tests

--- test/custom-date.js
+++ test/custom-date.js
@@ -1,13 +1,13 @@
 let assert = require('assert');
 
-let CustomDate = require('../build/custom-date');
+let CustomDate = require('../build/custom-date').default;
 
 describe('CustomDate', function() {
     describe('constructor', function() {
         specify('new CustomDate(year, month [, day [, hours [, minutes [, seconds]]]])', function() {
-            let date = new CustomDate(2020, 09);
-            assert(date.getFullYear() == 2020);
-            assert(date.getMonth() == 8);
+            let date = new CustomDate(2020, 9);
+            assert.equal(date.getFullYear(), 2020);
+            assert.equal(date.getMonth(), 8);
         });
         describe('new CustomDate(url)', function() {
             specify('timed', function() {
@@ -18,15 +18,20 @@ describe('CustomDate', function() {
                 assert.equal(date.getHours(), 10);
                 assert.equal(date.getMinutes(), 9);
                 assert.equal(date.getSeconds(), 22);
+                assert.equal(date.allday, false);
             });
             specify('allday', function() {
                 let date = new CustomDate('http://localhost:3004/1910/11/28');
                 assert.equal(date.getFullYear(), 1910);
                 assert.equal(date.getMonth(), 10);
                 assert.equal(date.getDate(), 28);
-                assert.equal(date.getHours(), 0);
-                assert.equal(date.getMinutes(), 0);
-                assert.equal(date.getSeconds(), 0);
+                assert.equal(date.allday, true);
+            });
+            specify('month', function() {
+                let date = new CustomDate('http://localhost:3004/1910/11');
+                assert.equal(date.getFullYear(), 1910);
+                assert.equal(date.getMonth(), 10);
+                assert.equal(date.allday, true);
             });
         });
         describe('new CustomDate(file)', function() {
@@ -38,15 +43,14 @@ describe('CustomDate', function() {
                 assert.equal(date.getHours(), 10);
                 assert.equal(date.getMinutes(), 9);
                 assert.equal(date.getSeconds(), 22);
+                assert.equal(date.allday, false);
             });
             specify('allday', function() {
                 let date = new CustomDate('content/1910/11/28/allday.md');
                 assert.equal(date.getFullYear(), 1910);
                 assert.equal(date.getMonth(), 10);
                 assert.equal(date.getDate(), 28);
-                assert.equal(date.getHours(), 0);
-                assert.equal(date.getMinutes(), 0);
-                assert.equal(date.getSeconds(), 0);
+                assert.equal(date.allday, true);
             });
         });
         describe('new CustomDate(customDate)', function() {
@@ -59,6 +63,7 @@ describe('CustomDate', function() {
                 assert.equal(b.getHours(), 10);
                 assert.equal(b.getMinutes(), 9);
                 assert.equal(b.getSeconds(), 22);
+                assert.equal(b.allday, false);
             });
             specify('allday', function() {
                 let a = new CustomDate(1910, 11, 28);
@@ -66,9 +71,7 @@ describe('CustomDate', function() {
                 assert.equal(b.getFullYear(), 1910);
                 assert.equal(b.getMonth(), 10);
                 assert.equal(b.getDate(), 28);
-                assert.equal(b.getHours(), 0);
-                assert.equal(b.getMinutes(), 0);
-                assert.equal(b.getSeconds(), 0);
+                assert.equal(b.allday, true);
             });
         });
     });
