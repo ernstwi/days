d38f74a6d1278d6d893303022055c3aa35e60e70
Ernst Widerberg <ernstwi@kth.se>
2022-11-30 18:24:49 +0100

    Fix

--- src/struct.ts
+++ src/struct.ts
@@ -435,7 +435,12 @@ class Asset {
 
     // Copy from altPath to path
     write(): void {
-        fs.copyFileSync(this.altPath, this.path, fs.constants.COPYFILE_EXCL);
+        if (this.altPath !== undefined)
+            fs.copyFileSync(
+                this.altPath,
+                this.path,
+                fs.constants.COPYFILE_EXCL
+            );
     }
 
     get path(): string {
