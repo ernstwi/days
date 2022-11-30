aa272c0174df46b0b9d80904021dc788fab599d3
Ernst Widerberg <ernstwi@kth.se>
2022-11-30 15:35:49 +0100

    merge.imessage: Rewrite, change API

--- src/merge/imessage.ts
+++ src/merge/imessage.ts
@@ -4,23 +4,15 @@ import os = require('os');
 import path = require('path');
 
 import parse = require('csv-parse/lib/sync');
-import pug = require('pug');
 
-import * as merge from './merge';
-import CustomDate from '../custom-date';
-import { NameCollision } from '../error';
-import { Post } from '../struct';
-
-let pugAsset = pug.compileFile(`${__dirname}/asset.pug`);
+import { Post, Asset } from '../struct';
 
 type csv = string[][];
 
-function mergeImessage(id: string, resolve: boolean) {
+function mergeImessage(id: string): [Post[], Asset[]] {
     // Collect data from Messages database
     let posts: Map<string, Post> = new Map();
-    let assets: Map<string, string[]> = new Map();
-
-    let data: any = {};
+    let assets: Map<string, Asset[]> = new Map();
 
     sqlite(id, 'time.sql').forEach(([id, time]) => {
         posts.set(id, new Post(false, new Date(Number(time) * 1000)));
@@ -36,10 +28,10 @@ function mergeImessage(id: string, resolve: boolean) {
         (posts.get(id) as Post).body = text; // Works because map stores Post by reference
     });
 
-    sqlite(id, 'asset.sql').forEach(([id, asset]) => {
-        asset = asset.replace(/^~/, os.homedir()); // Absoulte path of asset
+    sqlite(id, 'asset.sql').forEach(([id, file]) => {
+        file = file.replace(/^~/, os.homedir()); // Absoulte path of asset
         if (!assets.has(id)) assets.set(id, []);
-        (assets.get(id) as string[]).push(asset);
+        (assets.get(id) as Asset[]).push(new Asset(path.basename(file), file));
     });
 
     // Add assets to head of post text
@@ -47,54 +39,21 @@ function mergeImessage(id: string, resolve: boolean) {
         if (!assets.has(id)) continue;
 
         let assetsHeader =
-            (assets.get(id) as string[])
-                .map((a: string) => assetStr(path.basename(a)))
+            (assets.get(id) as Asset[])
+                .map((a: Asset) => a.htmlTag)
                 .join('\n') + '\n';
         if (post.body === '') post.body = assetsHeader;
         else post.body = [assetsHeader, post.body].join('\n');
     }
 
-    // Check for post collisions
-    let postCollisions = posts.values().filter(p => p.fileExists());
+    return [[...posts.values()], [...assets.values()].flatMap(x => x)];
 
-    // ---- Progress bar -------------------------------------------------------
-
-    // Merge assets
-    let substitutions = new Map<string, string>();
-    Object.values(data)
-        .filter((post: any) => post.assets !== undefined)
-        .forEach((post: any) => {
-            post.assets.forEach((asset: asset) => {
-                let newDst = '';
-                try {
-                    newDst = merge.mergeAsset(asset.src, asset.dst, resolve);
-                } catch (err) {
-                    if (!(err instanceof NameCollision)) throw err;
-                    console.error(`Name collision: ${err.message}`);
-                }
-                if (newDst !== asset.dst) substitutions.set(asset.dst, newDst);
-            });
-        });
-
-    // Merge posts
-    Object.values(data).forEach((post: any) => {
-        // Special case for entries consisting of ' ' with no assets.
-        if (post.text === ' ' && post.assets === undefined) return;
-
-        post.dst = post.date.file();
-        try {
-            merge.mergePost(
-                post.text,
-                post.dst,
-                post.date,
-                post.date,
-                substitutions
-            );
-        } catch (err) {
-            if (!(err instanceof NameCollision)) throw err;
-            console.error(`Name collision: ${err.message}`);
-        }
-    });
+    // TODO
+    // Everything after this should be same for iMessage and path
+    // 1. Check for collisions
+    // let postCollisions = posts.values().filter(p => p.fileExists());
+    // let assetCollisions = assets.values().filter(a => a.fileExists());
+    // 2. Report collisions, or do merge
 }
 
 // Run sqlite template `file` with `$ID` = `id`.
@@ -106,15 +65,6 @@ function sqlite(id: string, file: string): csv {
     return parse(
         cp.execSync(`sqlite3 -csv ~/Library/Messages/chat.db`, { input: sql })
     );
-}
-
-// Return an html tag for displaying file `filename`.
-function assetStr(filename: string) {
-    let extension = path.extname(filename).substring(1).toLowerCase();
-    return pugAsset({
-        extension: extension,
-        filename: filename
-    });
 }
 
 export default mergeImessage;
